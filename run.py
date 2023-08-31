10c6e242dcdecd08bc9700c5049cf0c46243c049
Tejas Vedagiri <tejasvedagiri1223@gmail.com>
2023-08-31 13:06:19 -0400

    Logging

--- run.py
+++ run.py
@@ -11,4 +11,5 @@ app.include_router(data_loader)
 async def get_documentation(request: Request):
     root_path = request.scope.get("root_path", "").rstrip("/")
     openapi_url = root_path + app.openapi_url
+    print(openapi_url)
     return get_swagger_ui_html(openapi_url=openapi_url, title="Swagger")
